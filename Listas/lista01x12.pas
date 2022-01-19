3e5cdeb6c4ade6dbdd737d3b996257af7900be79
gianluca-magnabosco <gianluca_1406@hotmail.com>
2022-01-19 07:00:32 -0300

    trabalhos feitos na disciplina

--- Listas/lista01x12.pas
+++
@@ -1,9 +0,0 @@
-Program tlista01x12;
-// 12)	FUP que mostre a soma de todos os números inteiros entre 1 e 10. 
-
-VAR	A,B,res:integer;
-Begin
-	FOR A:=1 TO 10 DO 
-		res:=res+A;
-	writeln(res);		
-End.
